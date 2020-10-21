c120cfb0a4f45e7f770ada995981f9b7327e54cb
Tiger Kong <tig567899@Computer.localdomain>
2020-10-20 21:41:18 -0400

    Fixed human movement

--- games/onitama.py
+++ games/onitama.py
@@ -210,24 +210,24 @@ class Card():
         self.colour = colour
         self.deltas = deltas
 CARDS = [
-    Card("Tiger", -1, [(2, 0), (-1, 0)]),
-    Card("Dragon", 1, [(1, -2), (1, 2), (-1, -1), (-1, 1)]),
-    Card("Crab", -1, [(1, 0), (0, 2), (0, -2)]),
-    Card("Elephant", 1, [(1, -1), (1, 1), (0, -1), (0, 1)]),
-    Card("Monkey", -1, [(-1, -1), (1, -1), (-1, 1), (1, 1)]),
-    Card("Mantis", 1, [(1, 1), (1, -1), (-1, 0)]),
-    Card("Crane", -1, [(1 , 0), (-1, 1), (-1, -1)]),
-    Card("Boar", 1, [(0, -1), (0, 1), (1, 0)]),
+    Card("Tiger", 1, [(2, 0), (-1, 0)]),
+    Card("Dragon", -1, [(1, -2), (1, 2), (-1, -1), (-1, 1)]),
+    Card("Crab", 1, [(1, 0), (0, 2), (0, -2)]),
+    Card("Elephant", -1, [(1, -1), (1, 1), (0, -1), (0, 1)]),
+    Card("Monkey", 1, [(-1, -1), (1, -1), (-1, 1), (1, 1)]),
+    Card("Mantis", -1, [(1, 1), (1, -1), (-1, 0)]),
+    Card("Crane", 1, [(1 , 0), (-1, 1), (-1, -1)]),
+    Card("Boar", -1, [(0, -1), (0, 1), (1, 0)]),
     # left-leaning
-    Card("Frog", 1, [(-1, -1), (0, 2), (1, 1)]),
-    Card("Goose", -1, [(1, 1), (0, -1), (0, 1), (-1, -1)]),
-    Card("Horse", 1, [(0, 1), (-1, 0), (1, 0)]),
-    Card("Eel", -1, [(-1, 1), (0, -1), (1, 1)]),
+    Card("Frog", -1, [(-1, -1), (0, 2), (1, 1)]),
+    Card("Goose", 1, [(1, 1), (0, -1), (0, 1), (-1, -1)]),
+    Card("Horse", -1, [(0, 1), (-1, 0), (1, 0)]),
+    Card("Eel", 1, [(-1, 1), (0, -1), (1, 1)]),
     # right-leaning
-    Card("Rabbit", -1, [(-1, 1), (0, -2), (1, -1)]),
-    Card("Rooster", 1, [(1, -1), (0, -1), (0, 1), (-1, 1)]),
-    Card("Ox", -1, [(0, -1), (-1, 0), (1, 0)]),
-    Card("Cobra", 1, [(-1, -1), (0, 1), (1, -1)]),
+    Card("Rabbit", 1, [(-1, 1), (0, -2), (1, -1)]),
+    Card("Rooster", -1, [(1, -1), (0, -1), (0, 1), (-1, 1)]),
+    Card("Ox", 1, [(0, -1), (-1, 0), (1, 0)]),
+    Card("Cobra", -1, [(-1, -1), (0, 1), (1, -1)]),
 ]
 
 class Onitama:
@@ -440,7 +440,7 @@ class Onitama:
         if target == "piece":
             return self.board[x][y] < 0
         elif target == "destination":
-            self.board[x][y] >= 0 
+            self.board[x][y] >= 0
         return True
 
     def action_to_human_input(self, action):
